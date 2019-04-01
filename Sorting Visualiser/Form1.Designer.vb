a1b7b0ddf339183caae1ed140a13d884ff94667e
PI_Gorbo_Laptop <sam.gorbwork@gmail.com>
2019-04-01 16:43:10 +1100

    Bubble Sort

--- Sorting Visualiser/Form1.Designer.vb
+++ Sorting Visualiser/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,81 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Btn_Rand = New System.Windows.Forms.Button()
+        Me.LstBx = New System.Windows.Forms.ListBox()
+        Me.ChkBx_Delay = New System.Windows.Forms.CheckBox()
+        Me.Btn_BubbleSort = New System.Windows.Forms.Button()
+        Me.Btn_SelectSort = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Btn_Rand
+        '
+        Me.Btn_Rand.Location = New System.Drawing.Point(247, 82)
+        Me.Btn_Rand.Name = "Btn_Rand"
+        Me.Btn_Rand.Size = New System.Drawing.Size(113, 54)
+        Me.Btn_Rand.TabIndex = 1
+        Me.Btn_Rand.Text = "Randomise Order"
+        Me.Btn_Rand.UseVisualStyleBackColor = True
+        '
+        'LstBx
+        '
+        Me.LstBx.FormattingEnabled = True
+        Me.LstBx.ItemHeight = 16
+        Me.LstBx.Location = New System.Drawing.Point(703, 12)
+        Me.LstBx.Name = "LstBx"
+        Me.LstBx.Size = New System.Drawing.Size(594, 692)
+        Me.LstBx.TabIndex = 2
+        '
+        'ChkBx_Delay
+        '
+        Me.ChkBx_Delay.AutoSize = True
+        Me.ChkBx_Delay.Enabled = False
+        Me.ChkBx_Delay.Location = New System.Drawing.Point(172, 184)
+        Me.ChkBx_Delay.Name = "ChkBx_Delay"
+        Me.ChkBx_Delay.Size = New System.Drawing.Size(291, 21)
+        Me.ChkBx_Delay.TabIndex = 4
+        Me.ChkBx_Delay.Text = "Show Each Compaison with a 0.25s delay"
+        Me.ChkBx_Delay.UseVisualStyleBackColor = True
+        '
+        'Btn_BubbleSort
+        '
+        Me.Btn_BubbleSort.Location = New System.Drawing.Point(96, 276)
+        Me.Btn_BubbleSort.Name = "Btn_BubbleSort"
+        Me.Btn_BubbleSort.Size = New System.Drawing.Size(113, 54)
+        Me.Btn_BubbleSort.TabIndex = 5
+        Me.Btn_BubbleSort.Text = "Bubble Sort"
+        Me.Btn_BubbleSort.UseVisualStyleBackColor = True
+        '
+        'Btn_SelectSort
+        '
+        Me.Btn_SelectSort.Location = New System.Drawing.Point(404, 276)
+        Me.Btn_SelectSort.Name = "Btn_SelectSort"
+        Me.Btn_SelectSort.Size = New System.Drawing.Size(113, 54)
+        Me.Btn_SelectSort.TabIndex = 6
+        Me.Btn_SelectSort.Text = "Selection Sort"
+        Me.Btn_SelectSort.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1309, 719)
+        Me.Controls.Add(Me.Btn_SelectSort)
+        Me.Controls.Add(Me.Btn_BubbleSort)
+        Me.Controls.Add(Me.ChkBx_Delay)
+        Me.Controls.Add(Me.LstBx)
+        Me.Controls.Add(Me.Btn_Rand)
+        Me.Name = "Form1"
         Me.Text = "Form1"
-    End Sub
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents Btn_Rand As Button
+    Friend WithEvents LstBx As ListBox
+    Friend WithEvents ChkBx_Delay As CheckBox
+    Friend WithEvents Btn_BubbleSort As Button
+    Friend WithEvents Btn_SelectSort As Button
 End Class
--- Sorting Visualiser/Form1.vb
+++ Sorting Visualiser/Form1.vb
@@ -1,3 +1,85 @@
 ﻿Public Class Form1
+    Dim nums(40) As Integer
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        For i = 1 To 40
+            nums(i) = i
+        Next
+
+        Display()
+
+    End Sub
+
+    Private Sub Display()
+        LstBx.Items.Clear()
+        For i = 1 To 40
+            LstBx.Items.Add(nums(i))
+        Next
+    End Sub
+
+    Private Sub Btn_Rand_Click(sender As Object, e As EventArgs) Handles Btn_Rand.Click
+
+        Dim max_index As Integer = (nums.Length - 1)
+        Dim j As Integer
+        Dim rnd As New Random
+
+        For i = 1 To (max_index)
+
+            'Get a radonom index
+            j = rnd.Next(1, max_index)
+
+            Dim temp As Integer = nums(i)
+            nums(i) = nums(j)
+            nums(j) = temp
+        Next
+        Display()
+
+    End Sub
+
+    Private Sub Btn_BubbleSort_Click(sender As Object, e As EventArgs) Handles Btn_BubbleSort.Click
+
+        Dim comparisons As Integer = 0
+        Dim count As Integer = nums.Length - 1
+        Dim temp As Integer
+
+        While 1 <> count
+
+            For i = 1 To (count - 1)
+
+                If nums(i) > nums(i + 1) Then
+                    temp = nums(i)
+                    nums(i) = nums(i + 1)
+                    nums(i + 1) = temp
+
+                Else
+                    Continue For
+                End If
+                comparisons += 1
+
+            Next
+
+            count -= 1
+
+        End While
+        Display()
+        LstBx.Items.Add("Comparisons: " + Convert.ToString(comparisons))
+
+    End Sub
+
+    Private Sub Btn_SelectSort_Click(sender As Object, e As EventArgs) Handles Btn_SelectSort.Click
+
+        Dim count As Integer = 1
+        Dim temp As Integer
+        Dim comparisons As Integer = 0
+        Dim small As Integer
+
+        While count <> (nums.Length - 1)
+            For i = count To nums.Length - 1
+
+            Next
+        End While
+
+
+    End Sub
 End Class
